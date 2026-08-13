f5caef7a50aa30391538c107cf13ab59421f2dac
kennzuki <kingsyken@gmail.com>
2026-08-13 15:33:35 +0300

    feat:notfound error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
+import { notFoundHandler } from "./middleware/notFound";
 
 export function createApp() {
 
   const app = express();
   app.use(express.json());
   app.use(errorHandler);
+  app.use(notFoundHandler);
 }
 
 

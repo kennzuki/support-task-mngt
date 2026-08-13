412bdeeb9d97fc51f59f3056be09b5fc1db2a4f0
kennzuki <kingsyken@gmail.com>
2026-08-13 13:48:30 +0300

    chore:setting up pino logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,15 @@
+import pino from "pino";
+import { env } from "../config/env";
+
+export const logger = pino({
+    level: env.LOG_LEVEL,
+    transport: 
+        env.isProduction ? undefined : {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+            },
+        },
+    },
+);

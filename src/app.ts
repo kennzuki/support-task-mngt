b44b3e80642847290126a6c575f28c16b2168f06
kennzuki <kingsyken@gmail.com>
2026-08-13 15:48:08 +0300

    feat:cors installed and added and url encoded

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
 import { notFoundHandler } from "./middleware/notFound";
+import cors from "cors";
 
 export function createApp() {
 
   const app = express();
+  app.use(cors());
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(errorHandler);
   app.use(notFoundHandler);
 }
